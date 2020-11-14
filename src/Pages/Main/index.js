dfbdd0739f0e29e68aca68468fce3bacfd872f19
FilipeConde <filipecondep@gmail.com>
2020-11-14 09:02:19 -0300

    First commit

---
+++ src/Pages/Main/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Main = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>main hello world</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+  },
+  label: {
+    fontSize: 20,
+  },
+});
+
+export default Main;
---
+++ src/index.js
@@ -0,0 +1,76 @@
+/* eslint-disable no-alert */
+/* eslint-disable react-native/no-inline-styles */
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View, Text, Button, FlatList} from 'react-native';
+
+const App = () => {
+  const addEntry = () => {
+    //console.log('eita');
+    alert('Abrir tela de acicionar lançamento!');
+  };
+
+  return (
+    <View style={{padding: 10}}>
+      <Text
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Saldo: $2.102,45
+      </Text>
+      <Button onPress={addEntry} title="Adicionar" />
+
+      <Text
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Categorias
+      </Text>
+
+      <FlatList
+        data={[
+          {key: 'Alimentação: $200'},
+          {key: 'Combustível: $12'},
+          {key: 'Aluguel: $120'},
+          {key: 'Lazer: $250'},
+          {key: 'Outros: $1200'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
+
+      <Text
+        style={{
+          fontSize: 22,
+          fontWeight: 'bold',
+          marginTop: 10,
+          marginBottom: 10,
+        }}>
+        Últimos lançamentos
+      </Text>
+
+      <FlatList
+        data={[
+          {key: 'Padaria Asa Branca: $10'},
+          {key: 'Suermercado Isadora: $190'},
+          {key: 'Posto Ipiranga: $120'},
+        ]}
+        renderItem={({item}) => <Text>{item.key}</Text>}
+      />
+    </View>
+  );
+};
+
+export default App;
