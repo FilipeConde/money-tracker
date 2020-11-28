ad7355bfc07fc86904f1101a3c0f30c24bbb9c6a
FilipeConde <filipecondep@gmail.com>
2020-11-28 11:43:50 -0300

    create props for report page and adjust props from main page

--- src/Components/EntrySummary/EntrySummaryList/index.js
+++ src/Components/EntrySummary/EntrySummaryList/index.js
@@ -1,19 +1,17 @@
 import React from 'react';
 import {View, Text, StyleSheet, FlatList} from 'react-native';
 
-export default function EntrySummaryList() {
+export default function EntrySummaryList({entriesGrouped}) {
   return (
     <View style={styles.container}>
       <Text style={styles.label}>Categorias</Text>
       <FlatList
-        data={[
-          {key: 'Alimentação: $200'},
-          {key: 'Combustível: $12'},
-          {key: 'Aluguel: $120'},
-          {key: 'Lazer: $250'},
-          {key: 'Outros: $1200'},
-        ]}
-        renderItem={({item}) => <Text>{item.key}</Text>}
+        data={entriesGrouped}
+        renderItem={({item}) => (
+          <Text>
+            {item.description} - ${item.amount}
+          </Text>
+        )}
       />
     </View>
   );
--- src/Components/EntrySummary/index.js
+++ src/Components/EntrySummary/index.js
@@ -3,11 +3,11 @@ import {View, Text, StyleSheet} from 'react-native';
 import EntrySummaryList from './EntrySummaryList';
 import EntrySummaryChart from './EntrySummaryChart';
 
-export default function EntrySummart() {
+export default function EntrySummary({entriesGrouped}) {
   return (
     <View style={styles.container}>
       <EntrySummaryChart />
-      <EntrySummaryList />
+      <EntrySummaryList entriesGrouped={entriesGrouped} />
     </View>
   );
 }
